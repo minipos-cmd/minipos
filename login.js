5fb4629eb3b7ee210d58509736e0d6ca21c1fdec
minipos <andiyoyo120@gmail.com>
2026-01-23 06:31:31 +0700

    login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login() {
   const user = document.getElementById("username").value.trim();
   const pass = document.getElementById("password").value.trim();
 
-  if (user === "admin" && pass === "1234") {
+  if (user === "admin" && pass === "Mimin1234") {
     localStorage.setItem("login", "true");
     window.location.href = "https://minipos-cmd.github.io/minipos/dashboard.html";
   } else {
