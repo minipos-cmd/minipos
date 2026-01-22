ca9978ad2c61d1109d19cbcdb5f54e78a4378454
minipos <andiyoyo120@gmail.com>
2026-01-23 03:30:37 +0700

    login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function login() {
 
   if (user === "admin" && pass === "1234") {
     localStorage.setItem("login", "true");
-    window.location.href = "https://minipos-cmd.github.io/minipos/dashboard.html";
+    window.location.href = "https://minipos-cmd.github.io/dashboard.html";
   } else {
     alert("Username atau password salah!");
   }
