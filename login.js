9e4cd5dd9ed56228aac1172a7c2803abdeb97791
minipos <andiyoyo120@gmail.com>
2026-01-22 08:16:31 +0700

    login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "admin" && pass === "1234") {
+    localStorage.setItem("login", "true");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Username atau password salah");
+  }
+}
